26f12f5f8dedde77dc620209ec7094fc84ec6fee
Jacopo <33716357+jzagoli@users.noreply.github.com>
2022-10-14 16:02:10 +0200

    argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -2,47 +2,103 @@
 // Created by Jacopo on 12/10/2022.
 //
 #include <argparse/argparse.hpp>
+#include <filesystem>
 #include <string>
 
 int main(int argc, char* argv[]) {
-  argparse::ArgumentParser parser{"cmapd"};
-  parser.add_description("Capacitated Multi Agent Pickup and Delivery solver.");
+    using namespace std::string_literals;
+    argparse::ArgumentParser parser{"cmapd"};
+    parser.add_description("Capacitated Multi Agent Pickup and Delivery solver.");
+    parser.add_epilog("You must provide at least one option between generate or evaluate.");
 
-  parser.add_argument("map_path")
-      .help("The absolute path pointing to the map file. For example, /home/map.txt");
+    // --- Arguments to parse ---
+    parser.add_argument("map_path")
+        .help("The path pointing to the map file. For example, /home/map.txt");
 
-  parser.add_argument("--generate")
-      .help("Generate instances.")
-      .default_value(false)
-      .implicit_value(true);
+    parser.add_argument("--generate")
+        .help(
+            "Flag used to specify you want to generate instances. "
+            "If set you must provide the number of instances, agents and tasks.")
+        .implicit_value(true)
+        .default_value(false);
 
-  parser.add_argument("--agents")
-      .help("Specify the positive number of agents in the generated instances.")
-      .scan<'u', unsigned>();
+    parser.add_argument("--agents")
+        .help("Specify the positive number of agents in the generated instances.")
+        .metavar("N_AGENTS")
+        .scan<'u', unsigned>();
 
-  parser.add_argument("--tasks")
-      .help("Specify the positive number of tasks in the generated instances.")
-      .scan<'u', unsigned>();
+    parser.add_argument("--tasks")
+        .help("Specify the positive number of tasks in the generated instances.")
+        .metavar("N_TASKS")
+        .scan<'u', unsigned>();
 
-  parser.add_argument("--instances-output")
-      .help(
-          "Specify the path in which the output instances will be saved. "
-          "If missing, instances will be saved in a folder named 'output' "
-          "next to the executable.")
-      .default_value("output");
+    parser.add_argument("--instances")
+        .help("Specify the positive number of instances to generate.")
+        .metavar("N_INSTANCES")
+        .scan<'u', unsigned>();
 
-  parser.add_argument("--evaluate")
-      .help(
-          "Specify the path pointing to a directory containing the generated instances "
-          "and evaluate them.");
+    parser.add_argument("--instances-output")
+        .help("Specify the path in which the output instances will be saved.")
+        .metavar("OUT_PATH")
+        .default_value("output"s);
 
-  try {
-    parser.parse_args(argc, argv);
-  } catch (const std::runtime_error& err) {
-    std::cerr << err.what() << std::endl;
-    std::cerr << parser;
-    std::exit(1);
-  }
+    parser.add_argument("--evaluate")
+        .help(
+            "Specify the path pointing to a directory containing the generated instances "
+            "and evaluate them.")
+        .metavar("INSTANCES_PATH");
 
-  return 0;
+    parser.add_argument("--solver")
+        .help("Specify the type of solver to use when evaluating the instances. "
+            "Could be CBS or PBS.")
+        .metavar("SOLVER")
+        .default_value("CBS"s);
+
+    // --- Parsing arguments ---
+    try {
+        parser.parse_args(argc, argv);
+    } catch (const std::runtime_error& err) {
+        std::cerr << err.what() << "\n";
+        std::cerr << parser;
+        std::exit(EXIT_FAILURE);
+    }
+
+    // --- Handling arguments ---
+    if (parser["--generate"] == false && !parser.present("--evaluate")) {
+        std::cerr << "You must provide at least one option between generate or evaluate!\n";
+        std::exit(EXIT_FAILURE);
+    }
+    auto map_path = std::filesystem::path{parser.get("map_path")};
+    if (parser["--generate"] == true) {
+        auto instances_out_path = std::filesystem::path{parser.get("--instances-output")};
+        auto n_agents_opt = parser.present<unsigned>("--agents");
+        auto n_tasks_opt = parser.present<unsigned>("--tasks");
+        auto n_instances_opt = parser.present<unsigned>("--instances");
+        if (n_agents_opt && n_tasks_opt && n_instances_opt) {
+            // TODO: replace print with call to generate instances.
+            std::cout << "Generating " << n_instances_opt.value() << " instances with "
+                      << n_agents_opt.value() << " agents and " << n_tasks_opt.value()
+                      << " tasks on map " << map_path << " and saving them in "
+                      << instances_out_path << std::endl;
+        } else {
+            std::cerr << "If you want to generate instances, you must provide the number "
+                         "of instances, agents and tasks!\n";
+            std::exit(EXIT_FAILURE);
+        }
+    }
+    if (auto instances_in_path_opt = parser.present("--evaluate")) {
+        auto instances_in_path = std::filesystem::path{instances_in_path_opt.value()};
+        const std::string& solver = parser.get("--solver");
+        // TODO: replace print with call to solver
+        if (solver == "CBS") {
+            std::cout << "Solving instances in " << instances_in_path << " with CBS solver.\n";
+        } else if (solver == "PBS") {
+            std::cout << "Solving instances in " << instances_in_path << " with PBS solver.\n";
+        } else {
+            std::cerr << solver << " is not a known solver. Possible solver are: CBS, PBS (case sensitive).\n";
+            std::exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
 }
