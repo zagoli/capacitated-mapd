486dfb7c55d43728554ba57b9235c22e52bff11f
Jacopo <33716357+jzagoli@users.noreply.github.com>
2022-10-17 17:49:23 +0200

    created struct CmapdSolution

---
+++ src/CmapdSolution.h
@@ -0,0 +1,27 @@
+/**
+ * @file CmapdSolution.h
+ * @brief Contains the struct CmapdSolution.
+ * @author Jacopo Zagoli
+ * @version 1.0
+ * @date October, 2022
+ * @copyright 2022 Jacopo Zagoli, Davide Furlani
+ */
+
+#pragma once
+#include <vector>
+#include "Point.h"
+
+/**
+ * @struct CmapdSolution
+ * @brief Represents a solution to a CMAP instance.
+ */
+struct CmapdSolution {
+    /// A vector containing a path (vector of points) for every agent.
+    const std::vector<std::vector<Point>> paths;
+    /// The length of the longest path
+    const std::size_t makespan;
+    /// The sum of all paths lengths
+    const std::size_t cost;
+    /// Number of resolved conflicts
+    const int conflicts;
+};
