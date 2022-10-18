ef33251fb06b454601adfedf3c0652393f7e7228
Jacopo <33716357+jzagoli@users.noreply.github.com>
2022-10-18 15:21:33 +0200

    added struct Constraint

---
+++ src/Constraint.h
@@ -0,0 +1,25 @@
+/**
+ * @file Constraint.h
+ * @brief Contains the struct Constraint.
+ * @author Jacopo Zagoli
+ * @version 1.0
+ * @date October, 2022
+ * @copyright 2022 Jacopo Zagoli, Davide Furlani
+ */
+
+#pragma once
+#include "Point.h"
+
+/// @struct Constraint
+/// @brief This struct models a constraint that prohibits an agent from moving in a certain way.
+/// Formally this is an edge-constraint, but we also use it to model a vertex constraint.
+struct Constraint {
+    /// The constrained agent.
+    int agent;
+    /// The moment in time in which the agent is prohibited from doing a move.
+    int timestep;
+    /// The cell from which the agent can't move.
+    Point from_position;
+    /// The cell to which the agent can't arrive.
+    Point to_position;
+};
--- src/Point.h
+++ src/Point.h
@@ -3,7 +3,6 @@
 //
 
 #pragma once
-#include <cstddef>
 
 struct Point {
     int row;
