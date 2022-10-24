5e3aee1fc425fd4a8b8447bf268d1b1f4d5b8ec7
Jacopo <33716357+jzagoli@users.noreply.github.com>
2022-10-24 15:20:22 +0200

    refactor

--- src/AmbientMapInstance.cpp
+++ src/AmbientMapInstance.cpp
@@ -13,42 +13,43 @@
 
 namespace cmapd {
 
-
-AmbientMapInstance::AmbientMapInstance(const std::filesystem::path& path_to_map_instance, const std::filesystem::path& path_to_map) : AmbientMap(path_to_map) {
-    
-    //std::ifstream map_file{std::filesystem::absolute(path_to_map)};
+AmbientMapInstance::AmbientMapInstance(const std::filesystem::path& path_to_map_instance,
+                                       const std::filesystem::path& path_to_map)
+    : AmbientMap(path_to_map) {
+    // std::ifstream map_file{std::filesystem::absolute(path_to_map)};
     std::ifstream map_instance_file{path_to_map_instance};
     if (!map_instance_file) {
         throw std::runtime_error{path_to_map_instance.string() + ": file does non exist"};
     }
-    
-    int num_agents {0};
-    int num_tasks {0};
-    
+
+    int num_agents{0};
+    int num_tasks{0};
+
     map_instance_file >> num_agents;
     map_instance_file >> num_tasks;
-    
-    for(int i=0; i<num_agents; i++){
-        int row_pos {0};
-        int col_pos {0};
+
+    for (int i = 0; i < num_agents; i++) {
+        int row_pos{0};
+        int col_pos{0};
         map_instance_file >> row_pos;
         map_instance_file >> col_pos;
         m_grid[row_pos][col_pos] = 'a';
         m_agents.push_back(Point{row_pos, col_pos});
     }
-    
-    for(int i=0; i<num_tasks; i++){
-        int row_pos_start {0};
-        int col_pos_start {0};
-        int row_pos_goal {0};
-        int col_pos_goal {0};
+
+    for (int i = 0; i < num_tasks; i++) {
+        int row_pos_start{0};
+        int col_pos_start{0};
+        int row_pos_goal{0};
+        int col_pos_goal{0};
         map_instance_file >> row_pos_start;
         map_instance_file >> col_pos_start;
         m_grid[row_pos_start][col_pos_start] = 't';
         map_instance_file >> row_pos_goal;
         map_instance_file >> col_pos_goal;
         m_grid[row_pos_goal][col_pos_goal] = 't';
-        m_tasks.emplace_back(Point{row_pos_start, col_pos_start}, Point{row_pos_goal, col_pos_goal});
+        m_tasks.emplace_back(Point{row_pos_start, col_pos_start},
+                             Point{row_pos_goal, col_pos_goal});
     }
 }
 
@@ -65,16 +66,15 @@ int AmbientMapInstance::get_num_tasks() const {
 }
 
 bool AmbientMapInstance::is_valid_position(Point p) const {
-    return p.row >= 0 && p.row < this->get_rows_number() &&
-           p.col >= 0 && p.col < this->get_columns_number()
-           && m_grid[p.row][p.col] != '#';
+    return p.row >= 0 && p.row < this->get_rows_number() && p.col >= 0
+           && p.col < this->get_columns_number() && m_grid[p.row][p.col] != '#';
 }
 
 std::string AmbientMapInstance::to_string() const {
     std::string s;
-    for (int r=0; r<this->get_rows_number(); r++) {
-        for (int c=0; c<this->get_columns_number(); c++) {
-            if(m_grid[r][c] == 'O')
+    for (int r = 0; r < this->get_rows_number(); r++) {
+        for (int c = 0; c < this->get_columns_number(); c++) {
+            if (m_grid[r][c] == 'O')
                 s += ' ';
             else
                 s += m_grid[r][c];
@@ -84,7 +84,5 @@ std::string AmbientMapInstance::to_string() const {
     return s;
 }
 
-std::vector<std::pair<Point, Point>> AmbientMapInstance::get_tasks() const {
-    return m_tasks;
-}
+std::vector<std::pair<Point, Point>> AmbientMapInstance::get_tasks() const { return m_tasks; }
 }  // namespace cmapd
--- tests/test_map_instance_class.cpp
+++ tests/test_map_instance_class.cpp
@@ -9,43 +9,38 @@ namespace AmbientMapInstanceTest {
 using cmapd::AmbientMapInstance;
 
 TEST_CASE("test assign instance", "[instantiation_instance]") {
-    std::filesystem::path path_to_map{"/../../tests/data/test_maps.txt"};
-    std::filesystem::path path_to_instance{"/../../tests/data/test_instance.txt"};
+    std::filesystem::path path_to_map{"data/test_maps.txt"};
+    std::filesystem::path path_to_instance{"data/test_instance.txt"};
 
-    REQUIRE_THROWS_AS(AmbientMapInstance(std::filesystem::current_path() += path_to_instance, std::filesystem::current_path() += path_to_map), std::runtime_error);
+    REQUIRE_THROWS_AS(AmbientMapInstance(path_to_instance, path_to_map), std::runtime_error);
 
-    path_to_map = "/../../tests/data/test_map.txt";
-    REQUIRE_NOTHROW(AmbientMapInstance(std::filesystem::current_path() += path_to_instance, std::filesystem::current_path() += path_to_map));
+    path_to_map = "data/test_map.txt";
+    REQUIRE_NOTHROW(AmbientMapInstance(path_to_instance, path_to_map));
 }
 
 TEST_CASE("test get number of rows and columns of a instance", "[rows_and_columns_instance]") {
-    
-    std::filesystem::path path_to_map{"/../../tests/data/test_map.txt"};
-    std::filesystem::path path_to_instance{"/../../tests/data/test_instance.txt"};
-    AmbientMapInstance instance {std::filesystem::current_path() += path_to_instance, std::filesystem::current_path() += path_to_map};
-    
+    std::filesystem::path path_to_map{"data/test_map.txt"};
+    std::filesystem::path path_to_instance{"data/test_instance.txt"};
+    AmbientMapInstance instance{path_to_instance, path_to_map};
+
     REQUIRE(instance.get_rows_number() == 5);
     REQUIRE(instance.get_columns_number() == 5);
-
 }
 
 TEST_CASE("test get number of agents and tasks of a instance", "[agents_and_tasks_instance]") {
-    
-    std::filesystem::path path_to_map{"/../../tests/data/test_map.txt"};
-    std::filesystem::path path_to_instance{"/../../tests/data/test_instance.txt"};
-    AmbientMapInstance instance {std::filesystem::current_path() += path_to_instance, std::filesystem::current_path() += path_to_map};
-    
+    std::filesystem::path path_to_map{"data/test_map.txt"};
+    std::filesystem::path path_to_instance{"data/test_instance.txt"};
+    AmbientMapInstance instance{path_to_instance, path_to_map};
+
     REQUIRE(instance.get_num_agents() == 2);
     REQUIRE(instance.get_num_tasks() == 2);
-    
 }
 
 TEST_CASE("test if a point is valid in a instance", "[is_valid_point_instance]") {
-    
-    std::filesystem::path path_to_map{"/../../tests/data/test_map.txt"};
-    std::filesystem::path path_to_instance{"/../../tests/data/test_instance.txt"};
-    AmbientMapInstance instance {std::filesystem::current_path() += path_to_instance, std::filesystem::current_path() += path_to_map};
-    
+    std::filesystem::path path_to_map{"data/test_map.txt"};
+    std::filesystem::path path_to_instance{"data/test_instance.txt"};
+    AmbientMapInstance instance{path_to_instance, path_to_map};
+
     cmapd::Point invalid_point{};
     invalid_point.row = -1;
     invalid_point.col = -1;
@@ -73,25 +68,22 @@ TEST_CASE("test if a point is valid in a instance", "[is_valid_point_instance]")
 }
 
 TEST_CASE("test get list of all tasks of a instance", "[tasks_instance]") {
-    
-    std::filesystem::path path_to_map{"/../../tests/data/test_map.txt"};
-    std::filesystem::path path_to_instance{"/../../tests/data/test_instance.txt"};
-    AmbientMapInstance instance {std::filesystem::current_path() += path_to_instance, std::filesystem::current_path() += path_to_map};
-    
-    std::vector<std::pair<cmapd::Point, cmapd::Point>> expected_tasks {};
-    expected_tasks.emplace_back(cmapd::Point{1,2},cmapd::Point{3,2});
-    expected_tasks.emplace_back(cmapd::Point{3,1},cmapd::Point{3,3});
-    
+    std::filesystem::path path_to_map{"data/test_map.txt"};
+    std::filesystem::path path_to_instance{"data/test_instance.txt"};
+    AmbientMapInstance instance{path_to_instance, path_to_map};
+
+    std::vector<std::pair<cmapd::Point, cmapd::Point>> expected_tasks{};
+    expected_tasks.emplace_back(cmapd::Point{1, 2}, cmapd::Point{3, 2});
+    expected_tasks.emplace_back(cmapd::Point{3, 1}, cmapd::Point{3, 3});
+
     REQUIRE(expected_tasks == instance.get_tasks());
-    
 }
 
 TEST_CASE("test if to_string return the correct instance string", "[to_string_method_instance]") {
-    
-    std::filesystem::path path_to_map{"/../../tests/data/test_map.txt"};
-    std::filesystem::path path_to_instance{"/../../tests/data/test_instance.txt"};
-    AmbientMapInstance instance {std::filesystem::current_path() += path_to_instance, std::filesystem::current_path() += path_to_map};
-    
+    std::filesystem::path path_to_map{"data/test_map.txt"};
+    std::filesystem::path path_to_instance{"data/test_instance.txt"};
+    AmbientMapInstance instance{path_to_instance, path_to_map};
+
     REQUIRE(instance.to_string() == "#####\n ata \n# # #\n ttt \n#####\n");
 }
 }
