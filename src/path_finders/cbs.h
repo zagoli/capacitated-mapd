0210eb43c0e6f9a49e80395087155f257502a77e
Jacopo Zagoli <jacopo.zagoli@studenti.univr.it>
2022-11-10 10:24:59 +0100

    cbs doc

--- src/path_finders/cbs.h
+++ src/path_finders/cbs.h
@@ -10,6 +10,14 @@
 
 namespace cmapd::cbs {
 
+/**
+ * This function finds paths without conflicts for every agent using a Conflict Based Search.
+ * @param instance The ambient map instance on which we are operating.
+ * @param goal_sequences A vector containing a goal sequence for every agent.
+ * @return a solution, if found.
+ * @throws runtime_error if no solution is found.
+ * @see Conflict-Based Search For Optimal Multi-Agent Path Finding.
+ */
 CmapdSolution cbs(const AmbientMapInstance& instance, const std::vector<path_t>& goal_sequences);
 
 }
