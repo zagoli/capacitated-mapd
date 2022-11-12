51659bbde37f06316c2f4dd4f483cab9a1afd419
Jacopo Zagoli <jacopo.zagoli@studenti.univr.it>
2022-11-12 10:29:59 +0100

    simplified code

--- src/path_finders/pbs.cpp
+++ src/path_finders/pbs.cpp
@@ -16,28 +16,19 @@ CmapdSolution pbs(AmbientMapInstance instance, const std::vector<path_t>& goal_s
     std::vector<Constraint> constraints{};
     std::vector<path_t> paths{};
 
-    for (int i = 0; static_cast<size_t>(i) < goal_sequences.size();
-         ++i) {  // per ogni sequenza di goal (una per agente) calcolo il percorso con multi A*
+    for (int i = 0; static_cast<size_t>(i) < goal_sequences.size(); ++i) {
         path_t path = multi_a_star::multi_a_star(
             i, instance.agents().at(i), goal_sequences.at(i), instance, constraints);
         paths.push_back(path);
-        for (int t = 0; static_cast<size_t>(t) < path.size();
-             ++t) {  // una volta calcolato il percorso aggiungo i constraint per ogni altro agente
+        for (int t = 0; static_cast<size_t>(t) < path.size(); ++t) {
             Point p{path.at(t)};
             for (int a = i + 1; a < instance.num_agents(); ++a) {
-                constraints.emplace_back(Constraint{a, t, p, p});
-
-                Point up{p.row - 1, p.col};
-                if (instance.is_valid(up)) constraints.emplace_back(Constraint{a, t, up, p});
-
-                Point down{p.row + 1, p.col};
-                if (instance.is_valid(down)) constraints.emplace_back(Constraint{a, t, down, p});
-
-                Point right{p.row, p.col + 1};
-                if (instance.is_valid(right)) constraints.emplace_back(Constraint{a, t, right, p});
-
-                Point left{p.row, p.col - 1};
-                if (instance.is_valid(left)) constraints.emplace_back(Constraint{a, t, left, p});
+                for (moves_t moves{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}; const auto& move : moves) {
+                    Point from_where = p + move;
+                    if (instance.is_valid(from_where)) {
+                        constraints.emplace_back(Constraint{a, t, from_where, p});
+                    }
+                }
             }
             instance.wall(path.back());
         }
